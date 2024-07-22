d8dcb2ff86f9e0d6bdc4a9c16a558ad8630c5588
BPrasad2000 <buddhiprasad0784558@gmail.com>
2024-07-22 19:21:06 +0530

    Delete and Update a Book

--- backend/index.js
+++ backend/index.js
@@ -66,6 +66,49 @@ app.get('/books/:id',async (req,res)=>{
     }
 })
 
+//Route for update a book
+app.put('/books/:id',async(req,res)=>{
+    try {
+        if(
+            !req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear 
+        ){
+            return res.status(400).send({
+                message:'Send all required fields : titlr,author,publishYear'
+            })
+        }
+        
+        const {id} =req.params;
+        const result =await Book.findByIdAndUpdate(id, req.body);
+        
+        if(!result){
+            return res.status(404).send({message:'Book not found' })
+        }
+        return res.status(200).send({message:'Book updated successfully'});
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({message:error.message});
+    }
+})
+
+//route for delete a book
+app.delete('/books/:id',async(req,res)=>{
+    try {
+        
+        const {id} =req.params;
+        const result =await Book.findByIdAndDelete(id);
+        
+        if(!result){
+            return res.status(404).send({message:'Book not found' })
+        }
+        return res.status(200).send({message:'Book deleted successfully'});
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({message:error.message});
+    }
+})
+
 mongoose.connect(mongoDBURL)
 .then(()=>{
     console.log('App connected to database');
