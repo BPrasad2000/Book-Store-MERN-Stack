0b14d9b175ffd33403d2c75715ae957d99ca0701
BPrasad2000 <buddhiprasad0784558@gmail.com>
2024-07-22 18:54:15 +0530

    Get One Book by id

--- backend/index.js
+++ backend/index.js
@@ -52,6 +52,19 @@ app.get('/books',async (req,res)=>{
         res.status(500).send({message:error.message});
     }
 })
+//Route for get one books from database by id
+app.get('/books/:id',async (req,res)=>{
+    try {
+
+        const {id} =req.params;
+        const books =await Book.findById(id);
+
+        return res.status(200).json(books);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({message:error.message});
+    }
+})
 
 mongoose.connect(mongoDBURL)
 .then(()=>{
